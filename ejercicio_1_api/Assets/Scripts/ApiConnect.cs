9f117851b92219f67c300b9a535d65b607f4d307
Juan Fernando Velez <94977033+juvelez@users.noreply.github.com>
2022-05-11 10:44:07 -0500

    Update code documentation

--- ejercicio_1_api/Assets/Scripts/ApiConnect.cs
+++ ejercicio_1_api/Assets/Scripts/ApiConnect.cs
@@ -6,8 +6,14 @@ using SimpleJSON;
 
 public class ApiConnect : MonoBehaviour {
 
+    /// <summary>
+    /// Proyect ID used to retrieve the information to the API
+    /// </summary>
     const string PROYECT_ID = "623b791c49b1d8002e60cb0e";
 
+    /// <summary>
+    /// To store the reference for the output textfield
+    /// </summary>
     public Text outputTextField;
 
     /// <summary>
@@ -35,6 +41,7 @@ public class ApiConnect : MonoBehaviour {
     /// <summary>
     /// METHOD 2
     /// Using Unity Engine Wrappers, more optimized for games and multimedia apps, it's compatible with CoRutines
+    /// This method is called by the Button "Ejecutar"
     /// </summary>
     /// 
     public void GetInfoMethod2() {
@@ -52,6 +59,12 @@ public class ApiConnect : MonoBehaviour {
         }));
     }
 
+    /// <summary>
+    /// Async execution of the API data retrieving 
+    /// </summary>
+    /// <param name="form">Data/parameters to send to the API query</param>
+    /// <param name="onFinish">Calleback to excecute when the data arrives</param>
+    /// <returns></returns>
     static IEnumerator getData(WWWForm form, System.Action<bool,string> onFinish) {
 
         bool res;
@@ -70,6 +83,10 @@ public class ApiConnect : MonoBehaviour {
 
                 JSONNode itemsData = JSON.Parse(request.downloadHandler.text);
                 JSONNode items = itemsData["resultado"]["textosPersonalizados"];
+
+                //TODO: Verify the correct variable name, the request was to consult "txt_inicio2"
+                //      but that variable does not exists in the retrieved data, there is a variable called "txt_inicio3"
+                //      this situation must be verified.
 
                 foreach (JSONNode item in items) {
                     if (item["variable"] == "txt_inicio3") {
